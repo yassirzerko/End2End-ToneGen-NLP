e51bfad83fe810e8085e04b8ad5a0560a17e5c90
Yassir <yassirelaoufir@gmail.com>
2024-05-04 19:16:15 -0400

    Update server

--- flask_server.py
+++ flask_server.py
@@ -1,11 +1,16 @@
 import flask
 from flask import request
 from flask_cors import CORS
-from src.core.constants import FEATURE_FORMAT_CONSTANTS
+from src.core.constants import FEATURE_FORMAT_CONSTANTS, PATH_NAME_CONSTANTS
 from src.core.ml.ml_models_utils import MlModelsUtils
 from src.core.ml.ml_models_utils import NlpFeaturesUtils
 import os 
 import csv
+
+# This Python file implements a Flask web application that serves as an API for machine learning models.
+# It provides two main routes: '/' for retrieving data about the best-performing models, and '/predict' for making predictions based on input text.
+# The file includes functions to load necessary data, retrieve model information, and make predictions, along with initialization of Flask and Flask-CORS.
+# Require the outputs of the script src.scripts.ml.train_optimize_save_models
 
 app = flask.Flask(__name__)
 CORS(app)
@@ -13,19 +18,11 @@ CORS(app)
 w2v_converters = NlpFeaturesUtils.load_word2vec_converters()
 bert_converters = NlpFeaturesUtils.load_bert_converters()
 
-def get_idf_voc_path(split_idx) :
-    idf_file_path = os.path.join('datasets', f'split{split_idx}', 'tf-idf-data.json')
-    voc_file_path = os.path.join('datasets', f'split{split_idx}', 'voc.json')
-    
-    return idf_file_path, voc_file_path
-
-def get_best_models_data() :
+def get_best_models_data(training_summary_file_path) :
     """
     Returns:
     - A dictionary containing the best-performing model data for each combination of model and feature vector format.
     """
-    
-    training_summary_file_path = 'trained_models_data.csv'
     file = open(training_summary_file_path,'r')
     reader = csv.reader(file)
     next(reader) 
@@ -57,16 +54,12 @@ def get_models_data() :
     This route function returns data about the best-performing models for each combination of model and feature vector format.
     """
     
-    models_data = get_best_models_data()
+    models_data = get_best_models_data(PATH_NAME_CONSTANTS.TRAINED_MODELS_DATA_FILE)
     formatted_response = {}
     training_data = []
     for model_name in models_data :
         model_dict_data = {'model_name' : model_name}
         for feature_format in FEATURE_FORMAT_CONSTANTS.FEATURES_NAMES :
-            if feature_format not in  models_data[model_name] :
-                # for debug
-                # TODO : REMOVE
-                continue 
             model_dict_data[feature_format] = round(float(models_data[model_name][feature_format]['accuracy']),2)
         
         training_data.append(model_dict_data)
@@ -86,8 +79,8 @@ def make_prediction() :
     model_name = request.args['model_name']
     feature_vec = request.args['feature_format']
     input_text = request.args['input_text']
-    models_data = get_best_models_data()
-    idf_file_path, voc_file_path = get_idf_voc_path(models_data[model_name][feature_vec]['split_idx'])
+    models_data = get_best_models_data(PATH_NAME_CONSTANTS.TRAINED_MODELS_DATA_FILE)
+    idf_file_path, voc_file_path = NlpFeaturesUtils.get_idf_voc_path(models_data[model_name][feature_vec]['split_idx'])
     prediction = MlModelsUtils.use_trained_model_to_predict_tone(models_data[model_name][feature_vec]['path'], input_text, feature_vec, idf_file_path, voc_file_path, w2v_converters, bert_converters)
     return {'prediction' : prediction}
     
